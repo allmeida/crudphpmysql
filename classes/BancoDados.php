741024c630bacc033da777c4890c0ad20d6e6add
Francinildo Almeida <allmeida.fran@gmail.com>
2019-12-28 14:24:46 -0300

    Crud Basico, parte 1

---
+++ classes/BancoDados.php
@@ -0,0 +1,24 @@
+<?php
+
+class BancoDados {
+
+    public function getConexao() {
+
+        $conexao = null;
+
+        try {
+            $conexao = new PDO(
+                "mysql:host=localhost;dbname=contatos",
+                "root",
+                ""
+            );
+        } catch (PDOException $erro) {
+            echo "Erro na conexao: " . $erro->getMessage();
+        }
+
+        return $conexao;
+    }
+
+}
+
+?>
